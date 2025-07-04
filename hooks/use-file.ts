7477b3eb13814166eb1f9e8237bbd10c34efa1c7
neue.dev <modavid.1964@gmail.com>
2025-07-04 16:09:46 +0800

    Create loader for pfp; sync up applicant modal.

--- hooks/use-file.ts
+++ hooks/use-file.ts
@@ -1,7 +1,7 @@
 /**
  * @ Author: Mo David
  * @ Create Time: 2025-06-19 06:01:21
- * @ Modified time: 2025-07-04 14:09:09
+ * @ Modified time: 2025-07-04 16:00:52
  * @ Description:
  *
  * Properly handles dealing with files stored in GCS and their local state.
@@ -12,6 +12,7 @@ import { useCallback, useState } from "react";
 
 interface IUseFile {
   url: string;
+  loading: boolean;
   sync: () => Promise<void>;
 }
 
@@ -30,6 +31,7 @@ export const useFile = ({
   defaultUrl?: string;
 }): IUseFile => {
   const [url, setUrl] = useState(defaultUrl);
+  const [loading, setLoading] = useState(true);
 
   /**
    * Performs a sync by requesting for the hash of the file from the server.
@@ -43,20 +45,24 @@ export const useFile = ({
     // Something went wrong
     if (!success) {
       console.error("Could not fetch file.");
+      setLoading(false);
       return;
     }
 
     // File has not been uploaded by host / source
     if (empty) {
+      setLoading(false);
       return;
     }
 
     // Update url
     setUrl(`${process.env.NEXT_PUBLIC_API_URL}${route}?hash=${hash}`);
+    setLoading(false);
   }, [route]);
 
   return {
     url,
+    loading,
     sync: synchronize,
   };
 };
