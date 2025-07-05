144c1e430eee4dcafeae5bc12ee603ae14d4fac0
Andrew <119732698+andrewnms@users.noreply.github.com>
2025-07-05 16:32:51 +0800

    Revert "Bugfixes#1"

--- lib/utils/url-utils.ts
+++ lib/utils/url-utils.ts
@@ -63,8 +63,11 @@ export const isValidOptionalLinkedinURL = isValidOptionalSiteURL([
   "www.linkedin.com",
 ]);
 export const isValidOptionalCalendarURL = isValidOptionalSiteURL([
-  "calendar.app.google/",
-  "calendar.google.com/calendar/u/0/appointments",
+  "calendar.app.google.com",
+  "calendar.app.google",
+  "calendar.google.com",
+  "calendly.com",
+  "cal.com",
 ]);
 
 /**
