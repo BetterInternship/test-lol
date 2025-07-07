34d7ac6e9d28e3fbc4dd1327b5eb9e7d1a0a8fe5
User <119732698+andrewnms@users.noreply.github.com>
2025-07-07 10:09:25 +0800

    fix: Copy on the Sign in button and the Warning unverified banner

--- components/features/hire/header.tsx
+++ components/features/hire/header.tsx
@@ -112,7 +112,7 @@ export const ProfileButton = () => {
       className="h-10 border-gray-300 hover:bg-gray-50 "
       onClick={() => router.push("/login")}
     >
-      Employer Sign in
+      Already have an account? Sign in.
     </Button>
   );
 };
