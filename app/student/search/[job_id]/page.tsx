a736231c8612b66809cfca4f9250401da0bb8e71
neue.dev <modavid.1964@gmail.com>
2025-07-15 16:42:08 +0800

    Handle deleted jobs in UI for apps.

--- app/student/search/[job_id]/page.tsx
+++ app/student/search/[job_id]/page.tsx
@@ -39,6 +39,7 @@ import {
   JobHead,
   JobApplicationRequirements,
 } from "@/components/shared/jobs";
+import { Card } from "@/components/ui/our-card";
 
 /**
  * The individual job page.
@@ -135,6 +136,32 @@ export default function JobPage() {
           <p className="text-red-600 mb-4">
             Failed to load job: {job.error.message}
           </p>
+        </div>
+      </div>
+    );
+  }
+
+  if (!job.data) {
+    return (
+      <div className="h-screen bg-white flex justify-center py-6">
+        <div className="flex flex-col justify-start items-start gap-4">
+          <Button
+            variant="ghost"
+            onClick={() => router.back()}
+            className="flex items-center gap-2 text-gray-600 hover:text-gray-900 hover:bg-gray-100 px-3 py-2 transition-colors"
+          >
+            <ArrowLeft className="w-4 h-4" />
+            Back
+          </Button>
+          <Card>
+            <div className="text-left max-w-prose">
+              <p className="font-bold mb-1">Job does not exist.</p>
+              <p className="text-gray-700 text-xs">
+                If you are coming from a valid link, it is possible that the job
+                was deleted by the employer.
+              </p>
+            </div>
+          </Card>
         </div>
       </div>
     );
@@ -254,9 +281,9 @@ export default function JobPage() {
                       />
                       <JobMode mode={job.data.mode} />
                     </div>
-                    
+
                     <div className="border-t border-gray-200 my-8"></div>
-                    
+
                     <h2 className="text-2xl text-gray-700 mb-6 flex items-center gap-3">
                       Description
                     </h2>
@@ -267,7 +294,7 @@ export default function JobPage() {
                         </ReactMarkdown>
                       </div>
                     </div>
-                    
+
                     <div className="border-t border-gray-200 my-8"></div>
                     <h2 className="text-2xl text-gray-700 mb-6 flex items-center gap-3">
                       Requirements
