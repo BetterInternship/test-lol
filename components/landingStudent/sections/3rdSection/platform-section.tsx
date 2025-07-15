6c805c2c446c7b3dd26801e7918a22f1e990437d
User <119732698+andrewnms@users.noreply.github.com>
2025-07-15 21:35:45 +0800

    Fix: navbar, copywriting for section3

--- components/landingStudent/sections/3rdSection/platform-section.tsx
+++ components/landingStudent/sections/3rdSection/platform-section.tsx
@@ -3,22 +3,22 @@
 import { FeatureSteps } from "@/components/landingStudent/sections/3rdSection/feature-section"
 
 const features = [
-    { 
+  { 
     step: 'Step 1',
-    title: 'Find listings for school credit',
-    content: 'Find listings where you can get it credited with your school',
+    title: 'Find listings for school credit.',
+    content: 'Discover internships that count towards your school requirements and academic credit.',
     image: '/landingPage/2ndSec/popimage3.png'
   },
   { 
     step: 'Step 2', 
-    title: 'Get a response in a day',
-    content: 'Employers get notified every time an application goes in', 
+    title: 'Get a response in a day.',
+    content: 'Receive updates quickly—employers are notified instantly when you apply.',
     image: '/landingPage/2ndSec/popimage1.png'
   },
   { 
     step: 'Step 3',
-    title: 'Check Application process',
-    content: 'Check Application process whether bound for an interview or in review',
+    title: 'Check application progress.',
+    content: 'Track your application status, see if you’re up for an interview, or under review.',
     image: '/landingPage/2ndSec/popimage2.png'
   },
 ]
